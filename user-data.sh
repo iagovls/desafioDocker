0a005b12356dd017817b0dfd84acefe1f8e5812d
Iago Vitor Lima Silva <136721166+iagovls@users.noreply.github.com>
2024-12-12 10:32:43 -0300

    Update user-data.sh
    Conexão da instância com EFS

--- user-data.sh
+++ user-data.sh
@@ -14,3 +14,9 @@ curl -L https://github.com/docker/compose/releases/download/v2.30.3/docker-compo
 
 #Modificar a permissão do diretório
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+#Criar o diretório efs 
+sudo mkdir -p /efs
+
+#Conectar no AWS EFS pelo link disponibilizado 
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0fdad736ffd266e41.efs.us-east-1.amazonaws.com:/ efs
