745cf1b8dbe34571a1b0fabae7e6671ce212e1ec
Iago Vitor Lima Silva <136721166+iagovls@users.noreply.github.com>
2024-12-22 15:03:12 -0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -25,10 +25,9 @@ sudo systemctl enable docker
 sudo mkdir -p /efs
 
 #  Monta um sistema de arquivos da Amazon Elastic File System (EFS) no Linux
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-078e758228282bd37.efs.us-east-1.amazonaws.com:/ efs
+<link para anexar o sistema de arquivos EFS>
 
 # Cria e edita o arquivo docker-compose.yml
-# Alterar as configurações sobre o DB e o EFS conforme como foi criado na AWS
 cat <<EOF > /home/ec2-user/docker-compose.yml
 
 services:
@@ -39,38 +38,12 @@ services:
     ports:
       - "80:80"
     environment:
-      WORDPRESS_DB_HOST: "database-2.cx406gsimskz.us-east-1.rds.amazonaws.com:3306"
-      WORDPRESS_DB_USER: "admin"
-      WORDPRESS_DB_PASSWORD: "2xdLLFuFwpKVtgff5plw"
-      WORDPRESS_DB_NAME: "database_2"
+      WORDPRESS_DB_HOST: "<endpoint do banco de dados>:3306"
+      WORDPRESS_DB_USER: "<usuário do banco de dados>"
+      WORDPRESS_DB_PASSWORD: "<senha do banco de dados>"
+      WORDPRESS_DB_NAME: "<nome do banco de dados>"
     volumes:
       - /efs/wordpress:/var/www/html
-
-  db:
-    image: mysql:5.7
-    container_name: mysql
-    restart: always
-    environment:
-      MYSQL_ROOT_PASSWORD: "2xdLLFuFwpKVtgff5plw"
-      MYSQL_DATABASE: "database_2"
-      MYSQL_USER: "admin"
-      MYSQL_PASSWORD: "2xdLLFuFwpKVtgff5plw"
-    volumes:
-      - /efs/mysql:/var/lib/mysql
-
-volumes:
-  wordpress_data:
-    driver: local
-    driver_opts:
-      type: "nfs"
-      o: "addr=fs-078e758228282bd37.efs.us-east-1.amazonaws.com,rw,nfsvers=4.1"
-      device: ":/wordpress"
-  mysql_data:
-    driver: local
-    driver_opts:
-      type: "nfs"
-      o: "addr=fs-078e758228282bd37.efs.us-east-1.amazonaws.com,rw,nfsvers=4.1"
-      device: ":/mysql"
 
 EOF
 
